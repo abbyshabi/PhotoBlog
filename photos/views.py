7c518f56f6f7df319d3209b99e481b84c85d11cf
abby-shabi <abbyshabi@gmail.com>
2019-03-02 19:14:00 +0300

    added the photos function

--- photos/views.py
+++ photos/views.py
@@ -1,3 +1,9 @@
-from django.shortcuts import render
-
+from django.shortcuts import render,redirect
+from django.http import HttpResponse, Http404
+import datetime as dt
+from .models import Image,Category,Location
 # Create your views here.
+
+def photos(request):
+    images = Image.get_photos()
+    return render(request,'index.html',{"images":images})
