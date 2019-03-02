b40e4704a07331bca4da6ef0650aff386adeb4af
abby-shabi <abbyshabi@gmail.com>
2019-03-02 19:14:22 +0300

    added the search function

--- photos/views.py
+++ photos/views.py
@@ -7,3 +7,16 @@ from .models import Image,Category,Location
 def photos(request):
     images = Image.get_photos()
     return render(request,'index.html',{"images":images})
+
+def search_results(request):
+
+    if 'image' in request.GET and request.GET["image"]:
+        category = request.GET.get("image")
+        searched_images = Image.search_by_category(category)
+        message = f"{category}"
+
+        return render(request, 'search.html',{"message":message,"images": searched_images})
+
+    else:
+        message = "You haven't searched for any image"
+        return render(request, 'search.html',{"message":message})    
