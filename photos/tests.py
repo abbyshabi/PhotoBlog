ecf209666262350fe064d6533f902524e918963f
abby-shabi <abbyshabi@gmail.com>
2019-03-04 21:08:18 +0300

    modified test files

--- photos/tests.py
+++ photos/tests.py
@@ -6,20 +6,24 @@ import datetime as dt
 class CategoryTestClass(TestCase):
  # Set up method
     def setUp(self):
-        self.travel= Category(name ='travel')
+        self.type= Category(name ='travel')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.travel,Category))
-    
+        self.assertTrue(isinstance(self.type,Category))
+
+    def test_init(self):
+       
+        self.assertTrue(self.type.name == 'travel')
+
     def test_save_method(self):
-        self.travel.save_category()
+        self.type.save_category()
         categorys = Category.objects.all()
         self.assertTrue(len(categorys)> 0)
     
     def test_delete_method(self):
-        self.travel.save_category()
+        self.type.save_category()
         categorys = Category.objects.all()
-        self.travel.delete_category()
+        self.type.delete_category()
         categorys = Category.objects.all()
         self.assertTrue(len(categorys)==0)
 
@@ -27,25 +31,27 @@ class CategoryTestClass(TestCase):
 
 class LocationTestClass(TestCase):
     def setUp(self):
-        self.lagos= Location(name ='lagos')
+        self.place= Location(name ='lagos')
 
     def test_instance(self):
-        self.assertTrue(isinstance(self.lagos,Location))
-    
+        self.assertTrue(isinstance(self.place,Location))
+
+    def test_init(self):
+        self.assertTrue(self.place.name =='lagos')
+
     def test_save_method(self):
-        self.lagos.save_location()
+        self.place.save_location()
         locations = Location.objects.all()
         self.assertTrue(len(locations)> 0)
 
     def tearDown(self):
-        Category.objects.all().delete()
         Location.objects.all().delete()
-        Image.objects.all().delete()
+
 
     def test_delete_method(self):
-        self.lagos.save_location()
+        self.place.save_location()
         locations = Location.objects.all()
-        self.lagos.delete_location()
+        self.place.delete_location()
         locations = Location.objects.all()
         self.assertTrue(len(locations)==0)
 
@@ -54,7 +60,11 @@ class ImageTestCase(TestCase):
         """
         This will create a new image before each test case
         """
-        self.new_image = Image(name = "dee",description = "i can do it")
+        fun = Category(name = "funny")
+        fun.save()
+        lagos = Location(name = "Lagos")
+        lagos.save()
+        self.new_image = Image(name = "image",description = "h",location = lagos,category = fun)
     
     def tearDown(self):
         """
@@ -65,9 +75,27 @@ class ImageTestCase(TestCase):
         Location.objects.all().delete()
     
     def test_instance(self):
-        """
-        This will test whether the new image created is an instance of the Image class
-        """
+       
         self.assertTrue(isinstance(self.new_image, Image))
-
     
+    def test_save_image(self):
+       
+        self.new_image.save_image()
+        self.assertTrue(len(Image.objects.all()) > 0)
+    
+    def test_filter_by_location(self):
+        
+        self.new_image.save_image()
+        self.assertTrue(len(Image.filter_by_location("Lagos")) >0)
+
+    def test_delete_method(self):
+        self.new_image.save_image()
+        images = Image.objects.all()
+        self.new_image.delete_image()
+        images = Image.objects.all()
+        self.assertTrue(len(images)==0)
+    
+    def test_search_by_category(self):
+
+        self.new_image.save_image()
+        self.assertTrue(len(Image.search_by_category("funny"))>0)
